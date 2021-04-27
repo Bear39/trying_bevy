d50beaaa6d9b6c937094e90a880417555a835b69
Bear39 <bps.bimo@gmail.com>
2021-04-27 19:01:13 +0700

    - add velocity

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,45 @@ fn main() {
 
 //Creating Marker Component for Player
 struct Player;
-// struct Player2;
+
+struct Player2 {
+    velocity: f32,
+    direction: Vec2,
+}
+
+// impl Default for Player2 {
+//     fn default() -> Self {
+//         Self {
+//             direction: Vec2::new(0.0, 1.0).normalize(),
+//             velocity: 200.0,
+//         }
+//     }
+// }
+
+// enum Direction {
+//     Left,
+//     Up,
+//     Right,
+//     Down,
+// }
+//
+// impl Direction {
+//     fn opposite(self) -> Self {
+//         match self {
+//             Self::Left => Self::Right,
+//             Self::Right => Self::Left,
+//             Self::Up => Self::Down,
+//             Self::Down => Self::Up,
+//         }
+//     }
+// }
 
 //Creating material component for Player
 struct Material(Handle<ColorMaterial>);
+
+struct Velocity(f32);
+
+struct Name(String);
 
 //creating initial setup, with Commands as parameter, commands used as spawner
 fn setup(mut commands: Commands, mut material: ResMut<Assets<ColorMaterial>>) {
@@ -34,7 +69,10 @@ fn spawn_player(mut commands: Commands, materials: Res<Material>) {
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
-        .insert(Player);
+        .insert(Player)
+        .insert(Velocity(100.0))
+        .insert(Name("Bear".to_string()));
+
     // commands
     //     // here we maake Player2 as entity
     //     .spawn_bundle(SpriteBundle {
@@ -48,22 +86,37 @@ fn spawn_player(mut commands: Commands, materials: Res<Material>) {
 // Accessing player position from it's transform (included in spritebundle)
 fn player_movement(
     // Taking transform component from player entity
-    mut positions: Query<&mut Transform, With<Player>>,
+    mut movement_data: Query<(&mut Transform, &mut Velocity), With<Player>>,
+    time: Res<Time>,
     //mut positions2: Query<&mut Transform, With<Player2>>,
     input: Res<Input<KeyCode>>,
 ) {
-    for mut transform in positions.iter_mut() {
+    let delta_time = time.delta_seconds();
+    for (mut transform, mut velocity) in movement_data.iter_mut() {
         if input.pressed(KeyCode::Left) {
-            transform.translation.x -= 2.;
+            transform.translation.x -= delta_time * velocity.0;
         }
         if input.pressed(KeyCode::Right) {
-            transform.translation.x += 2.;
+            transform.translation.x += delta_time * velocity.0;
         }
         if input.pressed(KeyCode::Down) {
-            transform.translation.y -= 2.;
+            transform.translation.y -= delta_time * velocity.0;
         }
         if input.pressed(KeyCode::Up) {
-            transform.translation.y += 2.;
+            transform.translation.y += delta_time * velocity.0;
+        }
+        //speed up
+        if input.pressed(KeyCode::W) {
+            velocity.0 += 10.0;
+        }
+        //speed down
+        if input.pressed(KeyCode::S) {
+            velocity.0 -= 10.0;
+        }
+        // reset
+        if input.pressed(KeyCode::R) {
+            transform.translation = Vec3::ZERO;
+            velocity.0 = 100.0;
         }
     }
 
