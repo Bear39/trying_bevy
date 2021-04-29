11a7dbd60b25732361038e59b00689f15127ff3f
Bear39 <bps.bimo@gmail.com>
2021-04-29 16:04:07 +0700

    - adding rapier - make player2 (using rapier phyics)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use bevy::prelude::*;
+use bevy_rapier2d::{
+    physics::RapierPhysicsPlugin,
+    rapier::{
+        dynamics::{RigidBodyBuilder, RigidBodySet},
+        geometry::{ColliderBuilder, ColliderSet},
+    },
+    render::RapierRenderPlugin,
+};
 
 fn main() {
     //runtime
     App::build()
+        .insert_resource(WindowDescriptor {
+            title: "UlerKampret".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin)
+        //for showing rigid body
+        //.add_plugin(RapierRenderPlugin)
         // work only once
         .add_startup_system(setup.system())
-        .add_startup_stage("game_setup", SystemStage::single(spawn_player.system()))
+        .add_startup_stage("game_setup", SystemStage::single(spawn_players.system()))
         .add_system(player_movement.system())
         .run();
 }
@@ -20,25 +36,25 @@ struct Player {
 impl Default for Player {
     fn default() -> Self {
         Self {
-            velocity: 100.0,
+            velocity: 1.,
             direction: Direction::Up,
         }
     }
 }
 
-// struct Player2 {
-//     velocity: f32,
-//     direction: Direction,
-// }
+struct Player2 {
+    velocity: f32,
+    direction: Direction,
+}
 
-// impl Default for Player2 {
-//     fn default() -> Self {
-//         Self {
-//             direction: Up,
-//             velocity: 200.0,
-//         }
-//     }
-// }
+impl Default for Player2 {
+    fn default() -> Self {
+        Self {
+            direction: Direction::Up,
+            velocity: 1.0,
+        }
+    }
+}
 
 // PartialEq to compare (!=,==, etc)
 // Copy to copy value (dir = player.direction)
@@ -63,34 +79,49 @@ impl Direction {
 }
 
 //Creating material component for Player
-struct Material(Handle<ColorMaterial>);
+struct Material {
+    player1_material: Handle<ColorMaterial>,
+    player2_material: Handle<ColorMaterial>,
+}
 
 //creating initial setup, with Commands as parameter, commands used as spawner
 fn setup(mut commands: Commands, mut material: ResMut<Assets<ColorMaterial>>) {
     // insert (spawn) 2D camera, since this is 2D games we use orthographic camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     // spawn the material for player
-    commands.insert_resource(Material(material.add(Color::rgb(0.7, 0.7, 0.7).into())));
+    commands.insert_resource(Material {
+        player1_material: material.add(Color::rgb(0.7, 0.7, 0.7).into()),
+        player2_material: material.add(Color::rgb(0.7, 0.7, 0.0).into()),
+    });
+
+    //testing rapier
+    // Static rigid-body with a cuboid shape.
+    // let rigid_body1 = RigidBodyBuilder::new_static();
+    // let collider1 = ColliderBuilder::cuboid(5.0, 5.0);
+    // commands.spawn().insert_bundle((rigid_body1, collider1));
 }
 
-fn spawn_player(mut commands: Commands, materials: Res<Material>) {
+fn spawn_players(mut commands: Commands, materials: Res<Material>) {
     commands
         // here we maake Player as entity
         .spawn_bundle(SpriteBundle {
-            material: materials.0.clone(),
+            material: materials.player1_material.clone(),
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
         .insert(Player::default());
 
-    // commands
-    //     // here we maake Player2 as entity
-    //     .spawn_bundle(SpriteBundle {
-    //         material: materials.0.clone(),
-    //         sprite: Sprite::new(Vec2::new(20.0, 20.0)),
-    //         ..Default::default()
-    //     })
-    //     .insert(Player2);
+    commands
+        // here we maake Player as entity
+        .spawn_bundle(SpriteBundle {
+            material: materials.player2_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(Player2::default())
+        .insert(RigidBodyBuilder::new_dynamic())
+        // always use half of the sprite size
+        .insert(ColliderBuilder::cuboid(5.0, 5.0));
 }
 
 // Accessing player position from it's transform (included in spritebundle)
@@ -101,7 +132,6 @@ fn player_movement(
     //mut positions2: Query<&mut Transform, With<Player2>>,
     input: Res<Input<KeyCode>>,
 ) {
-    let delta_time = time.delta_seconds();
     let mut dir: Direction;
     for (mut player, mut transform) in movement_data.iter_mut() {
         //changing player direction
@@ -125,32 +155,33 @@ fn player_movement(
             player.direction = dir;
         }
 
+        let delta_time = time.delta_seconds();
         match player.direction {
             Direction::Up => {
-                transform.translation.y += delta_time * player.velocity;
+                transform.translation.y += 5. * player.velocity;
             }
             Direction::Down => {
-                transform.translation.y -= delta_time * player.velocity;
+                transform.translation.y -= 5. * player.velocity;
             }
             Direction::Left => {
-                transform.translation.x -= delta_time * player.velocity;
+                transform.translation.x -= 5. * player.velocity;
             }
             Direction::Right => {
-                transform.translation.x += delta_time * player.velocity;
+                transform.translation.x += 5. * player.velocity;
             }
         }
         //speed up
-        if input.pressed(KeyCode::W) {
-            player.velocity += 10.0;
+        if input.just_pressed(KeyCode::W) {
+            player.velocity += 0.5;
         }
         //speed down
-        if input.pressed(KeyCode::S) {
-            player.velocity -= 10.0;
+        if input.just_pressed(KeyCode::S) {
+            player.velocity -= 0.5;
         }
         // reset
         if input.pressed(KeyCode::R) {
             transform.translation = Vec3::ZERO;
-            player.velocity = 100.0;
+            player.velocity = 1.;
         }
     }
 
